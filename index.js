0672a56ab86026be77d3bb9bcf6135020036fd6a
jakob <jakob.germano@gmail.com>
2021-04-15 18:19:16 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(array) {
+    const result = array.find(array => (array.result === 'W'));
+    if(Boolean(result) === true) {
+        return result.year;
+    } else {
+        return undefined;
+    }
+}
